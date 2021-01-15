31e4efb12c40d9cc48c8b69ffe41876bef288c3e
Hrenaki <shikhovwtr@mail.ru>
2021-01-15 14:59:14 +0700

    Mesh was added

--- MCE/Mesh.cs
+++ MCE/Mesh.cs
@@ -231,4 +231,82 @@ namespace MCE
             this.dy = dy;
         }
     }
+    class Mesh2D
+    {
+        List<float> x, y;
+        public int LinesCount { get { return 2 * (x.Count + y.Count); } }
+
+        public Mesh2D()
+        {
+            x = new List<float>();
+            y = new List<float>();
+        }
+
+        public void BuildMesh(double minX, double maxX, double minY, double maxY,
+            double dx, double dy)
+        {
+            double t;
+            int i;
+            double specPoint = (minX < 0 ? Math.Ceiling(minX / dx) : Math.Floor(minX / dx)) * dx;
+
+            if(x.Count != 0)
+                x.Clear();
+
+            x.Add((float)minX);
+            if (Math.Abs(x[x.Count - 1] - specPoint) > 1E-10)
+                x.Add((float)specPoint);
+
+            for (t = specPoint + dx, i = 1; t <= maxX; t = specPoint + (++i) * dx)
+                x.Add((float)t);
+
+            if (Math.Abs(x[x.Count - 1] - maxX) > 1E-10)
+                x.Add((float)maxX);
+
+            if(y.Count != 0)
+                y.Clear();
+
+            specPoint = (minY < 0 ? Math.Ceiling(minY / dy) : Math.Floor(minY / dy)) * dy;
+
+            y.Add((float)minY);
+            if (Math.Abs(y[y.Count - 1] - specPoint) > 1E-10)
+                y.Add((float)specPoint);
+
+            for (t = specPoint + dy, i = 1; t <= maxY; t = specPoint + (++i) * dy)
+                y.Add((float)t);
+
+            if (Math.Abs(y[y.Count - 1] - maxY) > 1E-10)
+                y.Add((float)maxY);
+        }
+        public float[] ToVertexBuffer()
+        {
+            float[] vertexes = new float[4 * (x.Count + y.Count)];
+            int yCount = y.Count;
+            int xCount = x.Count;
+            int k;
+            for (k = 0; k < xCount; k++)
+            {
+                vertexes[4 * k] = x[k];
+                vertexes[4 * k + 1] = y[0];
+                vertexes[4 * k + 2] = x[k];
+                vertexes[4 * k + 3] = y[yCount - 1];
+            }
+            xCount *= 4;
+            for(k = 0; k < yCount; k++)
+            {
+                vertexes[xCount + 4 * k] = x[0];
+                vertexes[xCount + 4 * k + 1] = y[k];
+                vertexes[xCount + 4 * k + 2] = x[x.Count - 1];
+                vertexes[xCount + 4 * k + 3] = y[k];
+            }
+            return vertexes;
+        }
+
+        public uint[] ToElementBuffer()
+        {
+            uint[] lines = new uint[2 * (x.Count + y.Count)];
+            for (uint i = 0; i < lines.Length; i++)
+                lines[i] = i;
+            return lines;
+        }
+    }
 }
--- MCE/Window.cs
+++ MCE/Window.cs
@@ -16,7 +16,12 @@ namespace MCE
         ColorMap cl;
         float[] colors;
 
+        Mesh2D mesh2D;
+        float[] meshVertexes;
+        float meshHeight;
+
         int VBO;
+        int VBOMesh;
 
         int ColorBuffer;
         int VAOTriangles;
@@ -25,12 +30,14 @@ namespace MCE
         int VAOEdges;       
         int EBOEdges;
 
+        int VAOMesh;
+
         Shader shader;
 
         Matrix4 view, proj;
         float speed = 0.5f;
         float fov = 45.0f;
-        Vector3 position = new Vector3(0.0f, 0.0f, 3.0f);
+        Vector3 position = new Vector3(0.0f, 0.0f, 5.0f);
         Vector3 front = new Vector3(0.0f, 0.0f, -1.0f);
         Vector3 up = new Vector3(0.0f, 1.0f, 0.0f);
         Vector3 right = new Vector3(1.0f, 0.0f, 0.0f);
@@ -47,9 +54,12 @@ namespace MCE
         public Window(double[][] vertexes, int[][] elements, double[] values, int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
         {
             cl = new ColorMap(values);
+            mesh2D = new Mesh2D();
 
             double maxX = vertexes[0][0];
             double maxY = vertexes[0][1];
+            double minX = maxX;
+            double minY = maxY;
 
             double[] curVert;
             int i;
@@ -61,18 +71,22 @@ namespace MCE
                     maxX = curVert[0];
                 if (curVert[1] > maxY)
                     maxY = curVert[1];
+                if (curVert[0] < minX)
+                    minX = curVert[0];
+                if (curVert[1] < minY)
+                    minY = curVert[1];
             }
 
-            int pos = 0;
+            position.X = (float) ((minX + maxX) / 2.0);
+            position.Y = (float) ((minY + maxY) / 2.0);
+
             this.vertexes = new float[vertexes.Length * 2];
             for(i = 0; i < vertexes.Length; i++)
             {
-                this.vertexes[pos] = (float) (vertexes[i][0] / maxX);
-                this.vertexes[pos + 1] = (float) (vertexes[i][1] / maxY);
-                pos += 2;
+                this.vertexes[2 * i] = (float) (vertexes[i][0]);
+                this.vertexes[2 * i + 1] = (float) (vertexes[i][1]);
             }
 
-            pos = 0;
             elems = new uint[elements.Length * 3];
 
             int j, k, s;
@@ -85,10 +99,9 @@ namespace MCE
             {
                 numbers = elements[i];
                 Array.Sort(numbers);
-                elems[pos] = (uint)numbers[0];
-                elems[pos + 1] = (uint)numbers[1];
-                elems[pos + 2] = (uint)numbers[2];
-                pos += 3;
+                elems[3 * i] = (uint)numbers[0];
+                elems[3 * i + 1] = (uint)numbers[1];
+                elems[3 * i + 2] = (uint)numbers[2];
 
                 for (j = 1; j < numbers.Length; j++)
                 {
@@ -121,7 +134,7 @@ namespace MCE
             }
 
             edges = new uint[(vertexes.Length + elements.Length - 1) * 2];
-            pos = 0;
+            int pos = 0;
             for(i = 0; i < adjacencyList.Length; i++)
                 foreach(int v in adjacencyList[i])
                 {
@@ -130,15 +143,17 @@ namespace MCE
                     pos += 2;
                 }
 
-            colors = cl.ColorsToArray();            
+            colors = cl.ToColorBuffer();            
         }
-        public Window(int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
+        public Window(int width, int height, string title) 
+            : base(width, height, GraphicsMode.Default, title)
         { }
         protected override void OnLoad(EventArgs e)
         {
             GL.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
             VBO = GL.GenBuffer();
+            VBOMesh = GL.GenBuffer();
 
             VAOTriangles = GL.GenVertexArray();
             ColorBuffer = GL.GenBuffer();
@@ -146,6 +161,8 @@ namespace MCE
 
             VAOEdges = GL.GenVertexArray();            
             EBOEdges = GL.GenBuffer();
+
+            VAOMesh = GL.GenVertexArray();
 
             #region VAO for triangles
             GL.BindVertexArray(VAOTriangles);
@@ -173,7 +190,7 @@ namespace MCE
             // Drops ColorBuffer and sets VBO
             GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
 
-            #region VAO for lines
+            #region VAO for Edges
             GL.BindVertexArray(VAOEdges);
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, EBOEdges);
             GL.BufferData(BufferTarget.ElementArrayBuffer, edges.Length * sizeof(uint),
@@ -181,6 +198,16 @@ namespace MCE
             
             GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 
                 2 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            #endregion
+
+            #region VAO for Mesh
+            GL.BindVertexArray(VAOMesh);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBOMesh);
+            GL.BufferData(BufferTarget.ArrayBuffer, 0,
+                new float[] { }, BufferUsageHint.StreamDraw);
+            GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float,
+                false, 2 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
             #endregion
 
@@ -195,8 +222,8 @@ namespace MCE
 
             // Default view and proj matrixes
             view = Matrix4.LookAt(position, position + front, up);
-            proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), Width / Height,
-                0.01f, 100.0f);
+            proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(fov), 
+                Width / Height, 0.01f, 100.0f);
 
             base.OnLoad(e);
         }
@@ -208,17 +235,20 @@ namespace MCE
             viewUniformLoc = GL.GetUniformLocation(shader.Handle, "view");
             projUniformLoc = GL.GetUniformLocation(shader.Handle, "proj");
 
+            float k = Width / (float)Height;
             view = Matrix4.LookAt(position, position + front, up);
-            proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(fov), Width / Height,
-                0.01f, 100.0f);
+            proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(fov), 
+                k, 0.01f, 100.0f);
             GL.UniformMatrix4(viewUniformLoc, false, ref view);
             GL.UniformMatrix4(projUniformLoc, false, ref proj);
 
             switch (mode)
             {
                 case Mode.Edges:
-                    GL.BindVertexArray(VAOEdges);                    
+                    GL.BindVertexArray(VAOEdges);
+                    GL.LineWidth(3);
                     GL.DrawElements(PrimitiveType.Lines, edges.Length, DrawElementsType.UnsignedInt, 0);
+                    GL.LineWidth(1);
                     GL.BindVertexArray(0);
                     break;
                 case Mode.Faces:
@@ -227,6 +257,25 @@ namespace MCE
                     GL.BindVertexArray(0);
                     break;
             }
+            
+            double beta = Math.Asin(k / Math.Tan(MathHelper.DegreesToRadians(67.5)));
+            if (beta < 0)
+                beta = Math.PI + beta;
+            meshHeight = (float) (2.0 * Math.Abs(position.Z) * Math.Tan(beta) / k);
+            float meshWidth = meshHeight * k;
+            mesh2D.BuildMesh(position.X - meshWidth / 2.0, position.X + meshWidth / 2.0,
+                position.Y - meshHeight / 2.0, position.Y + meshHeight / 2.0, 
+                0.25, 0.25);
+
+            meshVertexes = mesh2D.ToVertexBuffer();
+
+            GL.BindVertexArray(VAOMesh);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBOMesh);
+            GL.BufferData(BufferTarget.ArrayBuffer, meshVertexes.Length * sizeof(float),
+                meshVertexes, BufferUsageHint.StreamDraw);
+            GL.DrawArrays(PrimitiveType.Lines, 0, mesh2D.LinesCount);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
 
             SwapBuffers();
             base.OnRenderFrame(e);
@@ -276,6 +325,15 @@ namespace MCE
 
             base.OnMouseWheel(e);
         }
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            if(e.Mouse.IsButtonDown(MouseButton.Left))
+            {
+                position.X -= e.XDelta / (float)Width * 1.5f;
+                position.Y += e.YDelta / (float)Height * 1.5f;
+            }
+            base.OnMouseMove(e);
+        }
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             if (Keyboard.GetState().IsKeyDown(Key.Escape))
@@ -286,6 +344,7 @@ namespace MCE
         {
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
             GL.DeleteBuffer(VBO);
+            GL.DeleteBuffer(VBOMesh);
             shader.Dispose();
 
             base.OnUnload(e);
