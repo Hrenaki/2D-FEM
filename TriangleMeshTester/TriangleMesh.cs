e619cb524a71d3f98741b465fd658f3e6521b32e
Hrenaki <shikhovwtr@mail.ru>
2022-03-07 15:01:58 +0700

    Fixed mistakes, added properties

--- TriangleMeshTester/TriangleMesh.cs
+++ TriangleMeshTester/TriangleMesh.cs
@@ -12,17 +12,23 @@ namespace TriangleMeshTester
         private List<int[]> elements = new List<int[]>();
         private List<int> border = new List<int>();
 
-        public double[][] Vertices => vertices.ToArray();
-        public int[][] Elements => elements.ToArray();
+        public int VertexCount => vertices.Count;
+        public IReadOnlyCollection<double[]> Vertices => vertices.AsReadOnly();
+        public double[][] VerticesArray => vertices.ToArray();
 
-        public int[] Border => border.ToArray();
+        public int ElementCount => elements.Count;
+        public IReadOnlyCollection<int[]> Elements => elements.AsReadOnly();
+        public int[][] ElementsArray => elements.ToArray();
+
+        public IReadOnlyCollection<int> Border => border.AsReadOnly();
+        public int[] BorderArray => border.ToArray();
 
         public Result TryAddPoint(double x, double y, double eps)
         {
             var result = new Result();
 
             var pointNotExists = !vertices.Exists(point => Math.Abs(point[0] - x) < eps && Math.Abs(point[1] - y) < eps);
-            if (!pointNotExists)
+            if (pointNotExists)
                 vertices.Add(new double[] { x, y });
 
             return new Result() { IsSuccessful = pointNotExists, Message = pointNotExists ? string.Empty : "Point already exists." };
@@ -32,6 +38,12 @@ namespace TriangleMeshTester
         {
             if (element.Length != 3)
                 return new Result() { IsSuccessful = false, Message = "Element isn't a triangle." };
+
+            foreach(var index in element)
+            {
+                if (index < 0 || index >= vertices.Count)
+                    return new Result() { IsSuccessful = false, Message = $"Vertex with index '{index}' doesn't exist." };
+            }
 
             var sortedElement = new int[element.Length];
             Array.Copy(element, sortedElement, element.Length);
@@ -45,7 +57,7 @@ namespace TriangleMeshTester
 
             var elementNotExists = !elements.Exists(e => e[0] == sortedElement[0] && e[1] == sortedElement[1] && e[2] == sortedElement[2]);
             if (elementNotExists)
-                elements.Add(sortedElement);
+                elements.Add(new int[] { sortedElement[0], sortedElement[1], sortedElement[2], 0 });
 
             return new Result() { IsSuccessful = elementNotExists, Message = elementNotExists ? string.Empty : "Element already exists." };
         }
@@ -54,8 +66,8 @@ namespace TriangleMeshTester
         {
             this.border.Clear();
 
-            if (border.Length < 2)
-                return new Result() { IsSuccessful = false, Message = "Border must contain 2 points at least." };
+            if (border.Length < 4)
+                return new Result() { IsSuccessful = false, Message = "Border must contain 4 points at least." };
 
             if (border[0] != border[border.Length - 1])
                 return new Result() { IsSuccessful = false, Message = "Border isn't closed." };
@@ -65,7 +77,7 @@ namespace TriangleMeshTester
             for(int i = 0; i < border.Length - 1; i++)
             {
                 edgeExists = false;
-                start = Math.Max(border[i], border[i + 1]);
+                start = Math.Min(border[i], border[i + 1]);
                 end = Math.Max(border[i], border[i + 1]);
 
                 foreach(var element in elements)
